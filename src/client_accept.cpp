7cbbdc2ba40bd971aaf503788021de2fbfe64f04
rishilss99 <rishil2008@gmail.com>
2025-02-20 19:29:08 -0800

    Handle APIVersions requests v9

--- src/client_accept.cpp
+++ src/client_accept.cpp
@@ -15,7 +15,7 @@ void handleClient(int client_fd)
     int32_t response_msg_size, throttle_time;
     int16_t error_code;
     int8_t array_len, tag_buffer;
-    response_msg_size = sizeof(int32_t) + sizeof(int16_t) + sizeof(int8_t) + sizeof(int16_t) * 3 + sizeof(int32_t);
+    response_msg_size = sizeof(int32_t) + sizeof(int16_t) + sizeof(int8_t) + sizeof(int16_t) * 3 + sizeof(int8_t) + sizeof(int32_t) + sizeof(int8_t);
     response_msg_size = htobe32(response_msg_size);
     send(client_fd, &response_msg_size, sizeof(response_msg_size), 0);
     send(client_fd, &request_corr_id, sizeof(request_corr_id), 0);
@@ -41,6 +41,9 @@ void handleClient(int client_fd)
         send(client_fd, &api_key, sizeof(api_key), 0);
         send(client_fd, &min_ver, sizeof(min_ver), 0);
         send(client_fd, &max_ver, sizeof(max_ver), 0);
+        // TagBuffer
+        tag_buffer = 0;
+        send(client_fd, &tag_buffer, sizeof(tag_buffer), 0);
     }
     // Throttle time
     throttle_time = htobe32(0);
